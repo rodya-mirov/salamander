34207e3ea9d6f6d5ffd9e5a17cbe387372bf1031
Richard Rast <richard.rast@gmail.com>
2022-01-16 22:51:26 -0500

    feat: basic physics engine for sala-mixd, it feels pretty good

--- sala-mixd/TODO.md
+++ sala-mixd/TODO.md
@@ -1,4 +1,7 @@
 Improvements:
-- Shadow at the root of the sprite
-- Collider based on the shadow (sprite offset)
+- Maybe a little artificial inertia for player / NPC movement, to make it feel a little more natural
+- Make shadow a circle (maybe use a sprite, maybe there's a programmatic way to do this)
+- Improve asset loading reuse, this is not scalable
 - Improve z-layering (based on y-position at root, I guess? Maybe a custom component?)
+
+Next features???
--- sala-mixd/src/main.rs
+++ sala-mixd/src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy::sprite::{MaterialMesh2dBundle, Mesh2dHandle};
 use bevy_rapier2d::prelude::*;
 
 fn main() {
@@ -31,7 +32,7 @@ fn animate_sprite_system(
 }
 
 // pixels per second of movement; this is in world space, not camera space
-const PLAYER_PIXELS_PER_SEC: f32 = 100128.0;
+const PLAYER_PIXELS_PER_SEC: f32 = 128.0;
 
 // scale is multiplicative, so this is applied additively to the log of the camera scale
 // so 1/e is "doubles in one second", 2/e is "quadruples in one second" and so on.
@@ -39,16 +40,10 @@ const CAMERA_ZOOM_SPEED_PER_SEC: f32 = 1.0;
 
 fn player_input(
     kb: Res<Input<KeyCode>>,
-    time: Res<Time>,
-    mut query: Query<
-        (
-            &mut RigidBodyVelocityComponent,
-            &RigidBodyMassPropsComponent,
-        ),
-        With<Player>,
-    >,
+    mut query: Query<&mut RigidBodyVelocityComponent, With<Player>>,
 ) {
-    let dist = time.delta().as_secs_f32() * PLAYER_PIXELS_PER_SEC;
+    // let dist = time.delta().as_secs_f32() * PLAYER_PIXELS_PER_SEC;
+    let dist = PLAYER_PIXELS_PER_SEC;
     let mut desired: Vec2 = Vec2::new(0.0, 0.0);
 
     if kb.pressed(KeyCode::Left) {
@@ -69,37 +64,26 @@ fn player_input(
         desired *= dist;
     }
 
-    for (mut rb_vel, rb_mprops) in query.iter_mut() {
-        let rb_vel: &mut RigidBodyVelocityComponent = &mut *rb_vel;
-        let rb_mprops: &RigidBodyMassPropsComponent = &*rb_mprops;
-        rb_vel.apply_impulse(rb_mprops, desired.into());
+    for mut rb_vel in query.iter_mut() {
+        let vel: &mut RigidBodyVelocityComponent = &mut *rb_vel;
+        vel.linvel = desired.into();
+        vel.angvel = 0.0;
     }
 }
 
 fn follow_player(
-    time: Res<Time>,
     mut qs: QuerySet<(
         QueryState<&Transform, With<Player>>,
         QueryState<&mut Transform, With<PlayerCamera>>,
     )>,
 ) {
-    let dist = time.delta().as_secs_f32() * 500000.0; // PLAYER_PIXELS_PER_SEC;
-
     let player_pos: Vec3 = match qs.q0().iter().next() {
         Some(t) => t.translation,
         None => return,
     };
 
     for mut camera_transform in qs.q1().iter_mut() {
-        let diff: Vec3 = player_pos - camera_transform.translation;
-        let length = diff.length();
-        if length < dist {
-            camera_transform.translation = player_pos;
-        } else {
-            let unit = diff / length;
-            let dest = unit * dist;
-            camera_transform.translation += dest;
-        }
+        camera_transform.translation = player_pos;
     }
 }
 
@@ -124,9 +108,19 @@ fn player_camera_control(
     }
 }
 
-fn cancel_rotation(mut query: Query<&mut RigidBodyPositionComponent, With<NoRotate>>) {
-    for mut rbpc in query.iter_mut() {
-        rbpc.position.rotation = Default::default();
+// TODO: make sure this is actually happening after the rapier pass, so we don't have frame lag
+fn cancel_rotation(
+    mut query: Query<
+        (
+            Option<&mut RigidBodyPositionComponent>,
+            Option<&mut RigidBodyVelocityComponent>,
+        ),
+        With<NoRotate>,
+    >,
+) {
+    for (rbpc, rbvc) in query.iter_mut() {
+        rbpc.map(|mut rbpc| rbpc.position.rotation = Default::default());
+        rbvc.map(|mut rbvc| rbvc.angvel = 0.0);
     }
 }
 
@@ -143,9 +137,23 @@ fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    setup_player_character(&mut commands, &*asset_server, &mut *texture_atlases);
-    setup_npc(&mut commands, &*asset_server, &mut *texture_atlases);
+    setup_player_character(
+        &mut commands,
+        &*asset_server,
+        &mut *texture_atlases,
+        &mut *meshes,
+        &mut *materials,
+    );
+    setup_npc(
+        &mut commands,
+        &*asset_server,
+        &mut *texture_atlases,
+        &mut *meshes,
+        &mut *materials,
+    );
 
     for x in -10..=10 {
         let y = 10;
@@ -170,30 +178,84 @@ fn setup_player_character(
     commands: &mut Commands,
     asset_server: &AssetServer,
     texture_atlases: &mut Assets<TextureAtlas>,
+    meshes: &mut Assets<Mesh>,
+    materials: &mut Assets<ColorMaterial>,
 ) {
     let texture_handle = asset_server.load("sprites/gabe-idle-run.png");
     let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 7, 1);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
-    let pos = Vec2::new(32.0, 32.0);
-
     let mut player = commands.spawn();
 
-    player
-        .insert_bundle(SpriteSheetBundle {
-            texture_atlas: texture_atlas_handle,
-            transform: Transform::from_xyz(pos.x, pos.y, 2.0),
-            ..Default::default()
-        })
-        .insert(Timer::from_seconds(0.1, true))
-        .insert(NoRotate)
-        .insert(Player);
+    player.insert(NoRotate).insert(Player);
 
-    setup_rigid_body(&mut player, pos.x, pos.y, 8.0);
+    let config = SpriteBodyConfig {
+        pos: Vec2::new(32.0, 32.0),
+        collider_radius: 8.0,
+        sprite_offset: Vec2::new(0.0, 8.0),
+        texture_atlas_handle,
+        shadow_mesh: meshes
+            .add(Mesh::from(bevy::prelude::shape::Quad::new(Vec2::new(
+                16.0, 16.0,
+            ))))
+            .into(),
+        shadow_color: materials.add(ColorMaterial::from(Color::BLACK)),
+    };
+
+    setup_sprited_body(&mut player, config);
 }
 
-fn setup_rigid_body(spawner: &mut bevy_ecs::system::EntityCommands, x: f32, y: f32, rad: f32) {
-    let pos = Vec2::new(x, y);
+fn setup_npc(
+    commands: &mut Commands,
+    asset_server: &AssetServer,
+    texture_atlases: &mut Assets<TextureAtlas>,
+    meshes: &mut Assets<Mesh>,
+    materials: &mut Assets<ColorMaterial>,
+) {
+    let texture_handle = asset_server.load("sprites/mani-idle-run.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 7, 1);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    let mut npc = commands.spawn();
+
+    npc.insert(NoRotate);
+
+    let config = SpriteBodyConfig {
+        pos: Vec2::new(0.0, 0.0),
+        collider_radius: 8.0,
+        sprite_offset: Vec2::new(0.0, 8.0),
+        texture_atlas_handle,
+        shadow_mesh: meshes
+            .add(Mesh::from(bevy::prelude::shape::Quad::new(Vec2::new(
+                16.0, 16.0,
+            ))))
+            .into(),
+        shadow_color: materials.add(ColorMaterial::from(Color::BLACK)),
+    };
+
+    setup_sprited_body(&mut npc, config);
+}
+
+#[derive(Clone)]
+struct SpriteBodyConfig {
+    // position in world space
+    pos: Vec2,
+    // collider will be a circle with this radius, centered at the pos
+    collider_radius: f32,
+    // offset above the shadow
+    sprite_offset: Vec2,
+    // handle to the main sprite's texture
+    texture_atlas_handle: Handle<TextureAtlas>,
+    // shadow stuff
+    shadow_mesh: Mesh2dHandle,
+    shadow_color: Handle<ColorMaterial>,
+}
+
+fn setup_sprited_body(
+    spawner: &mut bevy_ecs::system::EntityCommands,
+    sprite_config: SpriteBodyConfig,
+) {
+    let pos = sprite_config.pos;
 
     spawner
         .insert_bundle(RigidBodyBundle {
@@ -201,7 +263,7 @@ fn setup_rigid_body(spawner: &mut bevy_ecs::system::EntityCommands, x: f32, y: f
             position: pos.into(),
             damping: RigidBodyDamping {
                 linear_damping: 5.0,
-                angular_damping: 57891.0,
+                angular_damping: 1.0,
             }
             .into(),
             ccd: RigidBodyCcd {
@@ -212,10 +274,34 @@ fn setup_rigid_body(spawner: &mut bevy_ecs::system::EntityCommands, x: f32, y: f
             ..Default::default()
         })
         .insert_bundle(ColliderBundle {
-            shape: ColliderShape::ball(rad).into(),
+            shape: ColliderShape::ball(sprite_config.collider_radius).into(),
             ..Default::default()
         })
         .insert(ColliderPositionSync::Discrete);
+
+    spawner.with_children(|s: &mut ChildBuilder| {
+        let mut shadow = s.spawn();
+        // TODO: make shadow visible
+        shadow.insert_bundle(MaterialMesh2dBundle {
+            mesh: sprite_config.shadow_mesh,
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, -2.0)),
+            material: sprite_config.shadow_color,
+            ..Default::default()
+        });
+
+        let mut main_sprite = s.spawn();
+        main_sprite
+            .insert_bundle(SpriteSheetBundle {
+                texture_atlas: sprite_config.texture_atlas_handle,
+                transform: Transform::from_xyz(
+                    sprite_config.sprite_offset.x,
+                    sprite_config.sprite_offset.y,
+                    -1.0,
+                ),
+                ..Default::default()
+            })
+            .insert(Timer::from_seconds(0.1, true));
+    });
 }
 
 fn setup_square_static_body(spawner: &mut bevy_ecs::system::EntityCommands, pos: Vec2, rad: f32) {
@@ -256,34 +342,10 @@ fn setup_brick(
 
     npc.insert_bundle(SpriteSheetBundle {
         texture_atlas: texture_atlas_handle,
-        transform: Transform::from_xyz(pos.x, pos.y, 0.0),
+        transform: Transform::from_xyz(pos.x, pos.y, -10.0),
         ..Default::default()
     })
     .insert(NoRotate);
 
     setup_square_static_body(&mut npc, pos, 8.0);
-}
-
-fn setup_npc(
-    commands: &mut Commands,
-    asset_server: &AssetServer,
-    texture_atlases: &mut Assets<TextureAtlas>,
-) {
-    let texture_handle = asset_server.load("sprites/mani-idle-run.png");
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 7, 1);
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
-
-    let mut npc = commands.spawn();
-
-    let pos = Vec2::new(0.0, 0.0);
-
-    npc.insert_bundle(SpriteSheetBundle {
-        texture_atlas: texture_atlas_handle,
-        transform: Transform::from_xyz(pos.x, pos.y, 1.0),
-        ..Default::default()
-    })
-    .insert(NoRotate)
-    .insert(Timer::from_seconds(0.1, true));
-
-    setup_rigid_body(&mut npc, pos.x, pos.y, 8.0);
 }
