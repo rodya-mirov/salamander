b61208809872ab3954c152507417ac0073cecdab
Richard Rast <richard.rast@gmail.com>
2022-01-16 21:52:24 -0500

    wip: basic physics system

---
+++ sala-mixd/TODO.md
@@ -0,0 +1,4 @@
+Improvements:
+- Shadow at the root of the sprite
+- Collider based on the shadow (sprite offset)
+- Improve z-layering (based on y-position at root, I guess? Maybe a custom component?)
--- sala-mixd/src/main.rs
+++ sala-mixd/src/main.rs
@@ -1,14 +1,18 @@
 use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .add_system(bevy::input::system::exit_on_esc_system)
         .add_startup_system(setup)
+        .add_startup_system(turn_off_gravity)
         .add_system(player_input)
         .add_system(player_camera_control)
         .add_system(animate_sprite_system)
         .add_system(follow_player)
+        .add_system(cancel_rotation)
         .run();
 }
 
@@ -27,20 +31,25 @@ fn animate_sprite_system(
 }
 
 // pixels per second of movement; this is in world space, not camera space
-const PLAYER_PIXELS_PER_SEC: f32 = 128.0;
+const PLAYER_PIXELS_PER_SEC: f32 = 100128.0;
 
 // scale is multiplicative, so this is applied additively to the log of the camera scale
-// so 1/e is "doubles in one second", 2/e is "quadruples in one second" and so on
-// must be positive
+// so 1/e is "doubles in one second", 2/e is "quadruples in one second" and so on.
 const CAMERA_ZOOM_SPEED_PER_SEC: f32 = 1.0;
 
 fn player_input(
     kb: Res<Input<KeyCode>>,
     time: Res<Time>,
-    mut query: Query<&mut Transform, With<Player>>,
+    mut query: Query<
+        (
+            &mut RigidBodyVelocityComponent,
+            &RigidBodyMassPropsComponent,
+        ),
+        With<Player>,
+    >,
 ) {
     let dist = time.delta().as_secs_f32() * PLAYER_PIXELS_PER_SEC;
-    let mut desired: Vec3 = Vec3::new(0.0, 0.0, 0.0);
+    let mut desired: Vec2 = Vec2::new(0.0, 0.0);
 
     if kb.pressed(KeyCode::Left) {
         desired.x -= dist;
@@ -55,15 +64,15 @@ fn player_input(
         desired.y -= dist;
     }
 
-    if desired == Vec3::default() {
-        return;
+    if desired != Vec2::default() {
+        desired /= desired.length();
+        desired *= dist;
     }
 
-    desired /= desired.length();
-    desired *= dist;
-
-    for mut transform in query.iter_mut() {
-        transform.translation += desired;
+    for (mut rb_vel, rb_mprops) in query.iter_mut() {
+        let rb_vel: &mut RigidBodyVelocityComponent = &mut *rb_vel;
+        let rb_mprops: &RigidBodyMassPropsComponent = &*rb_mprops;
+        rb_vel.apply_impulse(rb_mprops, desired.into());
     }
 }
 
@@ -74,7 +83,7 @@ fn follow_player(
         QueryState<&mut Transform, With<PlayerCamera>>,
     )>,
 ) {
-    let dist = time.delta().as_secs_f32() * PLAYER_PIXELS_PER_SEC;
+    let dist = time.delta().as_secs_f32() * 500000.0; // PLAYER_PIXELS_PER_SEC;
 
     let player_pos: Vec3 = match qs.q0().iter().next() {
         Some(t) => t.translation,
@@ -115,6 +124,15 @@ fn player_camera_control(
     }
 }
 
+fn cancel_rotation(mut query: Query<&mut RigidBodyPositionComponent, With<NoRotate>>) {
+    for mut rbpc in query.iter_mut() {
+        rbpc.position.rotation = Default::default();
+    }
+}
+
+#[derive(Component)]
+struct NoRotate;
+
 #[derive(Component)]
 struct Player;
 
@@ -129,10 +147,23 @@ fn setup(
     setup_player_character(&mut commands, &*asset_server, &mut *texture_atlases);
     setup_npc(&mut commands, &*asset_server, &mut *texture_atlases);
 
+    for x in -10..=10 {
+        let y = 10;
+
+        let pos = Vec2::new((x as f32) * 16.0, (y as f32) * 16.0);
+        setup_brick(&mut commands, &*asset_server, &mut *texture_atlases, pos);
+    }
+
+    let mut camera = OrthographicCameraBundle::new_2d();
+    camera.orthographic_projection.scale = 0.75;
+
     // setup camera
-    commands
-        .spawn_bundle(OrthographicCameraBundle::new_2d())
-        .insert(PlayerCamera);
+    commands.spawn_bundle(camera).insert(PlayerCamera);
+}
+
+fn turn_off_gravity(mut config: ResMut<RapierConfiguration>) {
+    config.gravity = Vector::<f32>::default();
+    config.scale = 1.0;
 }
 
 fn setup_player_character(
@@ -144,13 +175,93 @@ fn setup_player_character(
     let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 7, 1);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
-    commands
-        .spawn_bundle(SpriteSheetBundle {
+    let pos = Vec2::new(32.0, 32.0);
+
+    let mut player = commands.spawn();
+
+    player
+        .insert_bundle(SpriteSheetBundle {
             texture_atlas: texture_atlas_handle,
+            transform: Transform::from_xyz(pos.x, pos.y, 2.0),
             ..Default::default()
         })
         .insert(Timer::from_seconds(0.1, true))
+        .insert(NoRotate)
         .insert(Player);
+
+    setup_rigid_body(&mut player, pos.x, pos.y, 8.0);
+}
+
+fn setup_rigid_body(spawner: &mut bevy_ecs::system::EntityCommands, x: f32, y: f32, rad: f32) {
+    let pos = Vec2::new(x, y);
+
+    spawner
+        .insert_bundle(RigidBodyBundle {
+            body_type: RigidBodyType::Dynamic.into(),
+            position: pos.into(),
+            damping: RigidBodyDamping {
+                linear_damping: 5.0,
+                angular_damping: 57891.0,
+            }
+            .into(),
+            ccd: RigidBodyCcd {
+                ccd_enabled: true,
+                ..Default::default()
+            }
+            .into(),
+            ..Default::default()
+        })
+        .insert_bundle(ColliderBundle {
+            shape: ColliderShape::ball(rad).into(),
+            ..Default::default()
+        })
+        .insert(ColliderPositionSync::Discrete);
+}
+
+fn setup_square_static_body(spawner: &mut bevy_ecs::system::EntityCommands, pos: Vec2, rad: f32) {
+    spawner
+        .insert_bundle(RigidBodyBundle {
+            body_type: RigidBodyType::Static.into(),
+            position: pos.into(),
+            damping: RigidBodyDamping {
+                linear_damping: 5.0,
+                angular_damping: 57891.0,
+            }
+            .into(),
+            ccd: RigidBodyCcd {
+                ccd_enabled: true,
+                ..Default::default()
+            }
+            .into(),
+            ..Default::default()
+        })
+        .insert_bundle(ColliderBundle {
+            shape: ColliderShape::ball(rad).into(),
+            ..Default::default()
+        })
+        .insert(ColliderPositionSync::Discrete);
+}
+
+fn setup_brick(
+    commands: &mut Commands,
+    asset_server: &AssetServer,
+    texture_atlases: &mut Assets<TextureAtlas>,
+    pos: Vec2,
+) {
+    let texture_handle = asset_server.load("tiles/Level.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(16.0, 16.0), 6, 1);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    let mut npc = commands.spawn();
+
+    npc.insert_bundle(SpriteSheetBundle {
+        texture_atlas: texture_atlas_handle,
+        transform: Transform::from_xyz(pos.x, pos.y, 0.0),
+        ..Default::default()
+    })
+    .insert(NoRotate);
+
+    setup_square_static_body(&mut npc, pos, 8.0);
 }
 
 fn setup_npc(
@@ -162,10 +273,17 @@ fn setup_npc(
     let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 7, 1);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
-    commands
-        .spawn_bundle(SpriteSheetBundle {
-            texture_atlas: texture_atlas_handle,
-            ..Default::default()
-        })
-        .insert(Timer::from_seconds(0.1, true));
+    let mut npc = commands.spawn();
+
+    let pos = Vec2::new(0.0, 0.0);
+
+    npc.insert_bundle(SpriteSheetBundle {
+        texture_atlas: texture_atlas_handle,
+        transform: Transform::from_xyz(pos.x, pos.y, 1.0),
+        ..Default::default()
+    })
+    .insert(NoRotate)
+    .insert(Timer::from_seconds(0.1, true));
+
+    setup_rigid_body(&mut npc, pos.x, pos.y, 8.0);
 }
